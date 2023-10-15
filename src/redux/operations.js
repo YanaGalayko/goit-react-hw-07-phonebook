8f7774dda0195e49e1fd429c7708d99b31db6434
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-10-15 17:14:36 +0200

    API

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://652bf794d0d1df5273eef9d2.mockapi.io';
+axios.defaults.baseURL = 'https://652c0048d0d1df5273ef05b8.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
